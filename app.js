c4dd38971a0938e43b8430a16c3502d11f71a5dd
isa0208 <isa0208@users.noreply.github.com>
2024-07-31 20:22:30 +1200

    initial repo

---
+++ app.js
@@ -0,0 +1,29 @@
+//Node.js app that uses the Express framework
+
+//Set up a basic Express web server that listens on port 3000
+//Logs a message to the browser console to indicate the server is running
+
+//This script sets up a simple web server to handle
+//HTTP requests on port 3000
+
+
+//Express variable imports the Express framework
+//App variable creates an instance of an Express application
+var express = require('express');
+var app = express();
+
+app.set('view engine','ejs');
+app.get('/', function (_req, res){
+    res.render("home");
+    });
+
+
+app.get('/auckland', function (_req, res){
+    res.render("auckland");
+    });
+   
+
+//App listens into port 3000
+//Starts the server and makes it ready to handle HTTP requests
+app.listen(3000);
+console.log('Node app is running without issues on port 3000');
